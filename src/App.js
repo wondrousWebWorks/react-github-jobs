a67fea0b4388d88cec070fba6c194caa77f6ccab
Johann Alberts <alberts.johann@gmail.com>
2021-04-04 23:49:24 +0100

    Install react-markdown, add bootstrap card with sections for title, image, apply url

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import { Container } from 'react-bootstrap';
+import { Container } from "react-bootstrap";
 
-import useFetchJobs from './useFetchJobs';
+import useFetchJobs from "./useFetchJobs";
 
-import './App.css';
+import Job from './components/job/job.component';
+
+import "./App.css";
 
 const App = () => {
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
   const { jobs, loading, error } = useFetchJobs();
 
   return (
     <Container>
-      { loading && <h1>Loading...</h1> }
-      { error && <h1>Error. Try refreshing the page.</h1> }
-      <h1>{ jobs.length }</h1>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try refreshing the page.</h1>}
+      {jobs.map((job) => (
+        <Job key={job.id} job={job} />
+      ))}
     </Container>
-  )
-}
+  );
+};
 
 export default App;
---
+++ src/components/job/job.component.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import { Badge, Card } from "react-bootstrap";
+import ReactMarkdown from "react-markdown";
+
+const Job = ({ job }) => (
+  <Card>
+    <Card.Body>
+      <div className="d-flex justify-content-between">
+        <div>
+          <Card.Title>
+            {job.title} -{" "}
+            <span className="text-muted font-weight-light">{job.company}</span>
+          </Card.Title>
+          <Card.Subtitle className="text-muted mb-2">
+            {new Date(job.created_at).toLocaleDateString()}
+          </Card.Subtitle>
+          <Badge variant="secondary" className="mr-2">
+            {job.type}
+          </Badge>
+          <Badge variant="secondary">{job.location}</Badge>
+          <div style={{ wordBreak: "break-all" }}>
+            <ReactMarkdown source={job.how_to_apply} />
+          </div>
+        </div>
+        <img
+          className="d-none d-md-block"
+          height="50"
+          alt={job.company}
+          src={job.company_logo}
+        />
+      </div>
+    </Card.Body>
+  </Card>
+);
+
+export default Job;
