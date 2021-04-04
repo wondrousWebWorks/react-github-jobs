15c4fef3462e9ed26cfbf4bba7d3911141384436
Johann Alberts <alberts.johann@gmail.com>
2021-04-04 09:35:11 +0100

    Create useFetchJobs function with scaffolding to return data later

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React from 'react';
+
+import { Container } from 'react-bootstrap';
+
+import useFetchJobs from './useFetchJobs';
+
 import './App.css';
 
-function App() {
-  return <h1>Hello World</h1>
+const App = () => {
+  const { jobs, loading, error } = useFetchJobs();
+
+  return (
+    <Container>
+      { loading && <h1>Loading...</h1> }
+      { error && <h1>Error. Try refreshing the page.</h1> }
+      <h1>{ jobs.length }</h1>
+    </Container>
+  )
 }
 
 export default App;
---
+++ src/useFetchJobs.js
@@ -0,0 +1,9 @@
+const useFetchJobs = (params, page) => (
+  {
+    jobs: [],
+    loading: false,
+    error: false
+  }
+);
+
+export default useFetchJobs;
