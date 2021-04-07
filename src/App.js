780137065d9d84ae88fea937f8abb9cc118f81ef
Johann Alberts <alberts.johann@gmail.com>
2021-04-07 22:23:23 +0100

    Add search form and functionality

--- src/App.js
+++ src/App.js
@@ -4,26 +4,37 @@ import { Container } from "react-bootstrap";
 
 import useFetchJobs from "./useFetchJobs";
 
-import Job from './components/job/job.component';
-import JobPagination from './components/job-pagination/job-pagination';
+import Job from "./components/job/job.component";
+import JobPagination from "./components/job-pagination/job-pagination";
 
 import "./App.css";
+import SearchForm from "./components/search-form/search-form.component";
 
 const App = () => {
   const [params, setParams] = useState({});
   const [page, setPage] = useState(1);
-  const { jobs, loading, error, hasNextPage } = useFetchJobs();
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
+
+  const handeParamChange = (e) => {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1);
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value };
+    });
+  };
 
   return (
     <Container className="my-4">
       <h1 className="mb-4">GitHub Jobs</h1>
-      <JobPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
+      <SearchForm params={params} onParamChange={handeParamChange} />
+      <JobPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try refreshing the page.</h1>}
       {jobs.map((job) => (
         <Job key={job.id} job={job} />
       ))}
-      <JobPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
+      <JobPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
     </Container>
   );
 };
